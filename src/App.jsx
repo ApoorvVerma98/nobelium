12baa13a6ac414d0fa09cef51dd6a107230d1185
ApoorvVerma98 <apoorv.verma9@gmail.com>
2023-03-21 18:27:42 +0530

    assignment-css-react

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";//This line imports the React library and the useState hook from the "react" package.
+import styles from "./styles.module.css";//This line imports a CSS file for styling the component. The styles are stored in an object called styles.
+import Header from "./Header";//This line imports another React component called "Header" from a file called "Header.js".
+import Left from "./left";//This line imports a React component called "Left" from a file called "left.js".
+import Right from "./right";//This line imports a React component called "Right" from a file called "right.js".
+
+
+function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
+  return (
+    <div>
+      <Header toggleMenu={toggleMenu} showMenu={showMenu} />
+      <section className={styles.content}>
+        <Left />
+        <Right />
+      </section>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/right.jsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css";
+
+export default function Right() {
+    return (
+      <div className={styles.right}>
+        <img src="https://via.placeholder.com/400x400?text=Lorem+Ipsum" alt="Place Holder" />
+      </div>
+    );
+  }
+  
